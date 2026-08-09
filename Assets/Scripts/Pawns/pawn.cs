20ef31baf7894d13af3492270b0697ee7aeedf24
Bladescribs <ndajaira@gmail.com>
2026-08-08 23:52:25 -0700

    Added rotation and movement controls

---
+++ Assets/Scripts/Pawns/pawn.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public abstract class pawn : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public float rotateSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public abstract void MoveUp();
+
+    public abstract void MoveDown();
+
+    public abstract void RotateCounterclockwise();
+
+    public abstract void RotateClockwise();
+
+}
